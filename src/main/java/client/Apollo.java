d6209e4aa38d9f639db25406fa36ebff65699b77
Aldox890 <aldo.plenzich@mail.polimi.it>
2020-03-21 15:50:03 +0100

    Test di ereditarietà delle classi

---
+++ src/main/java/client/Apollo.java
@@ -0,0 +1,8 @@
+package client;
+
+public class Apollo extends Player {
+
+    public void test(){
+        System.out.println("CIAO SONO APOLLO");
+    }
+}
--- src/main/java/client/App.java
+++ src/main/java/client/App.java
@@ -4,10 +4,14 @@ package client;
  * Hello world!
  *
  */
-public class App 
+public class App // !! QUESTO E' SOLO UN TEST !!
 {
+    static Player l1 [] = new Player [2]; // lista che utilizzeremo per memorizzare i player
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Apollo p1 = new Apollo(); // creo l'oggetto utilizzando il dio scelto, che eredita dalla classe Player
+        l1[0] = p1; // salvo L'oggetto di tipo Apollo in un vettore di Player
+        l1[0].test(); // se esiste la funzione test() in apollo, la utilizzo, altrimenti richiamo la rispettiva di Player.
     }
 }
