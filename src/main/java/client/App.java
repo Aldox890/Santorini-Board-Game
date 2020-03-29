8a670ea6ba86255d40dea408ae88c6907f694c9d
Aldox890 <aldo.plenzich@mail.polimi.it>
2020-03-29 15:14:54 +0200

    Implemented client/server connection

--- src/main/java/client/App.java
+++ src/main/java/client/App.java
@@ -14,15 +14,8 @@ import client.god.Apollo;
 public class App // !! QUESTO E' SOLO UN TEST !!
 {
     static Player l1 [] = new Player [2]; // lista che utilizzeremo per memorizzare i player
-
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        Apollo p1 = new Apollo(); // creo l'oggetto utilizzando il dio scelto, che eredita dalla classe Player
-        l1[0] = p1; // salvo L'oggetto di tipo Apollo in un vettore di Player
-        l1[0].test(); // se esiste la funzione test() in apollo, la utilizzo, altrimenti richiamo la rispettiva di Player.
-
-
-
+    public static void main( String[] args ) throws IOException {
+        Client client = new Client("127.0.0.1",26175);
+        client.startClient();
     }
 }
