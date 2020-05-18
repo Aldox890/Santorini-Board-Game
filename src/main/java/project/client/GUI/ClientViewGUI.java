4e085dc3e25dd158f673c39a9273f87a8d502da0
Aldox890 <aldo.plenzich@mail.polimi.it>
2020-05-18 16:44:37 +0200

    Added gameboard on registration

--- src/main/java/project/client/GUI/ClientViewGUI.java
+++ src/main/java/project/client/GUI/ClientViewGUI.java
@@ -18,6 +18,8 @@ public class ClientViewGUI implements Observer {
 
     Socket socket;
     ObjectOutputStream objectOutputStream;
+    JFrame loginFrame;
+    JFrame santoriniFrame;
 
     public ClientViewGUI(Socket socket) throws IOException {
         this.socket = socket;
@@ -27,7 +29,7 @@ public class ClientViewGUI implements Observer {
 
     public void login() throws IOException {
 
-        JFrame loginFrame = new JFrame("Santorini login");
+        loginFrame = new JFrame("Santorini login");
         JPanel loginPanel = new JPanel();
         JLabel username = new JLabel("Username:");
         JLabel age = new JLabel("Age:");
@@ -72,6 +74,28 @@ public class ClientViewGUI implements Observer {
             }
         });
     }
+    
+    public void createGameFrame() {
+        loginFrame.setVisible(false);
+        santoriniFrame = new JFrame("Santorini");
+        GameBoard gameBoard = new GameBoard();
+
+        santoriniFrame.getContentPane().add(gameBoard);
+
+        gameBoard.setSize(new Dimension(1280, 720));
+        santoriniFrame.setSize(1280, 758);
+        santoriniFrame.setResizable(false);
+
+        santoriniFrame.setVisible(true);
+
+
+        santoriniFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("X: " + e.getX() + "Y: " + e.getY());
+            }
+        });
+    }
 
     @Override
     public void update(Observable o, Object arg) {
@@ -85,6 +109,7 @@ public class ClientViewGUI implements Observer {
                 case (0): // required player registration
                     if (mex.getData().equals("registered")) {
                         System.out.println("Successfully registered!");
+                        createGameFrame();
                     } else if (mex.getErrorData() == null) {
                         login();
 
@@ -93,7 +118,6 @@ public class ClientViewGUI implements Observer {
                         //login();
                     }
                     break;
-
             }
         }
         catch (IOException e){
@@ -101,64 +125,4 @@ public class ClientViewGUI implements Observer {
         }
     }
 
-
-    //public void ClientViewGUI(){
-        /*
-        JFrame santoriniFrame = new JFrame("Santorini");
-        GameBoard gameBoard = new GameBoard();
-
-        JFrame loginFrame = new JFrame("Santorini login");
-        JPanel loginPanel = new JPanel();
-        JLabel username = new JLabel("Username:");
-        JLabel age = new JLabel("Age:");
-        JTextField usernameArea = new JTextField(10);
-        JTextField ageArea = new JTextField(2);
-        JButton loginButton = new JButton("Login");
-
-        loginPanel.setLayout(null);
-        loginFrame.setSize(new Dimension(400,140));
-        loginPanel.setSize(new Dimension(400,140));
-        loginPanel.setBackground(new Color(135, 177, 182));
-
-        username.setBounds(20,20,100,20);
-        age.setBounds(20,50,100,20);
-
-        usernameArea.setBounds(100,20,100,20);
-        ageArea.setBounds(100,50,100,20);
-
-        loginButton.setBounds(220,20,100,50);
-
-        loginPanel.add(loginButton);
-        loginPanel.add(age);
-        loginPanel.add(username);
-        loginPanel.add(usernameArea);
-        loginPanel.add(ageArea);
-        loginFrame.add(loginPanel);
-        loginFrame.setVisible(true);
-
-        loginButton.addMouseListener(new MouseAdapter() {
-                                            @Override
-                                            public void mousePressed(MouseEvent e) {
-                                                loginFrame.setVisible(false);
-                                                santoriniFrame.setVisible(true);
-                                            }
-                                        });
-
-        santoriniFrame.getContentPane().add(gameBoard);
-
-        gameBoard.setSize(new Dimension(1280,720));
-        santoriniFrame.setSize(1280, 758);
-        santoriniFrame.setResizable(false);
-
-        //santoriniFrame.setVisible(true);
-
-
-        santoriniFrame.addMouseListener(new MouseAdapter() {
-                                            @Override
-                                            public void mousePressed(MouseEvent e) {
-                                                System.out.println("X: " + e.getX() + "Y: " +e.getY());
-                                            }
-                                        });
-
-         */
 }
