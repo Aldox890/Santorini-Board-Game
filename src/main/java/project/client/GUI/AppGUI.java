e333d92343703b096c9facb775a3ded369025eaa
Aldox890 <aldo.plenzich@mail.polimi.it>
2020-05-18 15:46:42 +0200

    Added observer pattern on gui

---
+++ src/main/java/project/client/GUI/AppGUI.java
@@ -0,0 +1,25 @@
+package project.client.GUI;
+import project.client.ClientView;
+import project.client.ServerObserver;
+
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class AppGUI {
+        public static void main( String[] args ) throws IOException, ClassNotFoundException {
+
+            Socket socket = new Socket("localhost",26175); //82.48.25.174     127.0.0.1   95.248.176.145   87.8.115.142
+            System.out.println("Connection established");
+
+            ClientViewGUI clientViewGUI = new ClientViewGUI(socket);
+            ServerObserver serverObserver = new ServerObserver(socket);
+            serverObserver.addObserver(clientViewGUI);
+
+            clientViewGUI.login();
+
+            serverObserver.waitFromServer();
+
+
+    }
+}
--- src/main/java/project/client/GUI/ClientViewGUI.java
+++ src/main/java/project/client/GUI/ClientViewGUI.java
@@ -4,11 +4,68 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class ClientViewGUI {
+public class ClientViewGUI implements Observer {
 
-    private static  void ClientViewGUI(){
+    Socket socket;
+
+    public ClientViewGUI(Socket socket){
+        System.out.println("AA");
+        this.socket = socket;
+    }
+
+    public void login(){
+        JFrame loginFrame = new JFrame("Santorini login");
+        JPanel loginPanel = new JPanel();
+        JLabel username = new JLabel("Username:");
+        JLabel age = new JLabel("Age:");
+        JTextField usernameArea = new JTextField(10);
+        JTextField ageArea = new JTextField(2);
+        JButton loginButton = new JButton("Login");
+
+        loginPanel.setLayout(null);
+
+        loginFrame.setSize(new Dimension(400,140));
+        loginPanel.setSize(new Dimension(400,140));
+
+        loginPanel.setBackground(new Color(135, 177, 182));
+
+        username.setBounds(20,20,100,20);
+        age.setBounds(20,50,100,20);
+
+        usernameArea.setBounds(100,20,100,20);
+        ageArea.setBounds(100,50,100,20);
+
+        loginButton.setBounds(220,20,100,50);
+
+        loginPanel.add(loginButton);
+        loginPanel.add(age);
+        loginPanel.add(username);
+        loginPanel.add(usernameArea);
+        loginPanel.add(ageArea);
+        loginFrame.add(loginPanel);
+        loginFrame.setVisible(true);
+
+        loginButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                //Send login to server
+            }
+        });
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+
+    //public void ClientViewGUI(){
+        /*
         JFrame santoriniFrame = new JFrame("Santorini");
         GameBoard gameBoard = new GameBoard();
 
@@ -49,9 +106,6 @@ public class ClientViewGUI {
                                             }
                                         });
 
-
-
-
         santoriniFrame.getContentPane().add(gameBoard);
 
         gameBoard.setSize(new Dimension(1280,720));
@@ -67,9 +121,6 @@ public class ClientViewGUI {
                                                 System.out.println("X: " + e.getX() + "Y: " +e.getY());
                                             }
                                         });
-    }
 
-    public static void main(String[] args) {
-        ClientViewGUI();
-    }
+         */
 }
