08477346870c7351f194d5c03a9d41bff40734b9
Mattia <mattiasala09@gmail.com>
2020-05-22 01:29:33 +0200

    BoardPanel: set up of playing field with dome on lvl 3 + added dome graphic resources.

--- src/main/java/project/client/GUI/BoardPanel.java
+++ src/main/java/project/client/GUI/BoardPanel.java
@@ -1,7 +1,11 @@
 package project.client.GUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class BoardPanel extends JPanel {
@@ -20,15 +24,31 @@ public class BoardPanel extends JPanel {
         lim = new GridBagConstraints();
         this.setLayout(gridbaglayout);
 
-        for(int c=0;c<=4;c++){
-            for(int r=0;r<=4;r++){
-                JButton component = new JButton("B"+r+","+c);
+        for(int r=0;r<=4;r++){
+            for(int c=0;c<=4;c++){
+                //ImageIcon img1 = new ImageIcon(graphicsPath+"levelNumber\\3.png");
+                //ImageIcon img = new ImageIcon(graphicsPath+"levelNumber\\dome45.png");
+                BufferedImage combinedImage = null;
+                try {
+                    final BufferedImage bg = ImageIO.read(new File(graphicsPath+"levelNumber\\3.png"));
+                    final BufferedImage dome = ImageIO.read(new File(graphicsPath+"levelNumber\\dome45.png"));
+                    combinedImage = new BufferedImage(bg.getWidth(), bg.getHeight(), BufferedImage.TYPE_INT_ARGB );
+                    Graphics2D g = combinedImage.createGraphics();
+                    g.drawImage(bg,0,0,null);
+                    g.drawImage(dome,26,26,null);
+                    g.dispose();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                JButton component = new JButton("B"+r+","+c,new ImageIcon(combinedImage));
                 //component.setPreferredSize(new Dimension(102,102));
                 component.setContentAreaFilled(false);
                 component.setOpaque(false);
                 component.setBorder(null);
-                lim.gridx=r;
-                lim.gridy=c;
+                lim.gridx=c;
+                lim.gridy=r;
                 lim.weightx=1;
                 lim.weighty=1;
                 lim.fill = GridBagConstraints.BOTH;
